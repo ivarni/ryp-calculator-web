52e049f5a2eb337d621d719f431d21e0d2d2d827
Ivar Nilsen <post@ivarnilsen.com>
2016-08-27 13:56:53 +0200

    Handle history api

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var fallback = require('express-history-api-fallback');
 var path = require('path');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, 'dist')));
+const root = path.join(__dirname, 'dist');
+
+app.use(express.static(root));
+app.use(fallback('index.html', { root: root }));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
